58acd9dcbe34ea0c183301f82263a7618eeffb8d
Boris Mandzhukov <skipter@abv.bg>
2018-09-20 23:40:36 +0300

    Add Stack Fibonacci.

---
+++ C# Advanced/Stacks and Queues - Exercises/08.StackFibonacci/StackFibonacci.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _08.StackFibonacci
+{
+    class StackFibonacci
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
