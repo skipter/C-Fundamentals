8a55d408862da59c92baa47c0812e29bad9cd2a1
Boris Mandzhukov <skipter@abv.bg>
2018-09-21 23:27:11 +0300

    Stack and Queue exercises.

--- C# Advanced/Stacks and Queues - Exercises/08.StackFibonacci/StackFibonacci.cs
+++ C# Advanced/Stacks and Queues - Exercises/08.StackFibonacci/StackFibonacci.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _08.StackFibonacci
 {
@@ -7,8 +8,20 @@ namespace _08.StackFibonacci
         static void Main(string[] args)
         {
             int num = int.Parse(Console.ReadLine());
+            Stack<long> fibonacci = new Stack<long>();
 
+            fibonacci.Push(0);
+            fibonacci.Push(1);
 
+            for (int i = 0; i < num - 1; i++)
+            {
+                long firstNum = fibonacci.Pop();
+                long secondNum = fibonacci.Peek();
+
+                fibonacci.Push(firstNum);
+                fibonacci.Push(firstNum + secondNum);
+            }
+            Console.WriteLine(fibonacci.Peek());
         }
     }
 }
---
+++ C# Advanced/Stacks and Queues - Exercises/09.SimpleTextEditor/SimpleTextEditor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _09.SimpleTextEditor
+{
+    class SimpleTextEditor
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Stack<string> stack = new Stack<string>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(" ");
+                int operation = int.Parse(tokens[0]);
+
+                string text = String.Empty;
+                switch (operation)
+                {
+                    case 1:
+                        //Append
+                        string currentText = tokens[1];
+                        stack.Push(text);
+                        text += currentText;
+                        break;
+
+                    case 2:
+                        //Erase
+                        int count = int.Parse(tokens[1]);
+                        stack.Push(text);
+                        text = text.Substring(0, text.Length - count);
+                        break;
+
+                    case 3:
+                        //Return element at position
+                        int index = int.Parse(tokens[1]);
+                        Console.WriteLine(text[index - 1]);
+                        break;
+
+                    case 4:
+                        //Undoes
+                        text = stack.Pop();
+                        break;
+                }
+
+            }
+        }
+    }
+}
