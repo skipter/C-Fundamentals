8a2f3a2a3e4fa2cf35ebc6a826fcea66f3797dcd
Boris Mandzhukov <skipter@abv.bg>
2018-09-24 09:52:06 +0300

    Add multidimensional matrixes - exercises.

---
+++ C# Advanced/Multidimensional Arrays - Exercises/01.MatrixOfPalindromes/MatrixOfPalindromes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.MatrixOfPalindromes
+{
+    class MatrixOfPalindromes
+    {
+        static void Main()
+        {
+            //Get matrix sizes.
+            int[] dimensions = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(Int32.Parse)
+                .ToArray();
+
+            string[,] matrix = new string[dimensions[0], dimensions[1]];
+            
+            //Use arrray alphabet for the input.
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {   //The palindrome logic
+                    matrix[row, col] = "" + (char)('a' + row) + (char)('a' + row + col) + (char)('a' + row); //aaa aba aca ada aea afa
+                                                                                                             //bbb bcb bdb beb bfb bgb    and etc...
+                }
+            }
+
+            //Print the result
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/Multidimensional Arrays - Exercises/02.DiagonalDifference/DiagonalDifference.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.DiagonalDifference
+{
+    class DiagonalDifference
+    {
+        static void Main()
+        {
+            int dimensions = Int32.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[dimensions, dimensions];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(Int32.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+            //ToDo
+        }
+    }
+}
