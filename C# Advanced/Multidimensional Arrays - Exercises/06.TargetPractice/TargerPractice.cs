11647225ca0e64988f564cbc776ab0b5e8f275b6
Boris Mandzhukov <skipter@abv.bg>
2018-09-25 17:02:41 +0300

    matrix exercises.

--- C# Advanced/Multidimensional Arrays - Exercises/06.TargetPractice/TargerPractice.cs
+++ C# Advanced/Multidimensional Arrays - Exercises/06.TargetPractice/TargerPractice.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _06.TargetPractice
 {
@@ -6,7 +7,32 @@ namespace _06.TargetPractice
     {
         static void Main()
         {
-            
+            int[] dimensions = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(Int32.Parse)
+                .ToArray();
+
+            int rowSize = dimensions[0];
+            int colSize = dimensions[1];
+
+            int[][] matrix = new int[rowSize][];
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                //matrix[row] = matrix[colSize];
+                char[] snake = Console.ReadLine()
+                    .ToArray();
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+
+                }
+            }
+
+            foreach (var item in matrix)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
         }
     }
 }
