95d784929651cdbccff4cb851559937ea32db250
Boris Mandzhukov <skipter@abv.bg>
2018-09-26 21:20:11 +0300

    Finish first part with matrix and jagged arrays exercises.

--- C# Advanced/Multidimensional Arrays - Exercises/06.TargetPractice/TargerPractice.cs
+++ C# Advanced/Multidimensional Arrays - Exercises/06.TargetPractice/TargerPractice.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace _06.TargetPractice
@@ -11,28 +12,123 @@ namespace _06.TargetPractice
                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
                 .Select(Int32.Parse)
                 .ToArray();
+            string snake = Console.ReadLine();
+            int[] target = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(Int32.Parse)
+                .ToArray();
 
-            int rowSize = dimensions[0];
-            int colSize = dimensions[1];
+            int rows = dimensions[0];
+            int cols = dimensions[1];
 
-            int[][] matrix = new int[rowSize][];
+            char[][] matrix = new char[rows][];
+
+            GetMatrix(matrix, cols, snake);
+
+            Shoot(matrix, target);
+
+            Collpase(matrix);
+            Print(matrix);
+        }
+
+        private static void Collpase(char[][] matrix)
+        {
+            Queue<char> elements = new Queue<char>(matrix.Length);
+
+            for (int col = 0; col < matrix[0].Length; col++)
+            {
+                int counter = 0;
+
+                for (int row = 0; row < matrix.Length; row++)
+                {
+                    if (matrix[row][col] != ' ')
+                    {
+                        elements.Enqueue(matrix[row][col]);
+                    }
+                    else
+                    {
+                        counter++;
+                    }
+                }
+
+                for (int row = 0; row < counter; row++)
+                {
+                    matrix[row][col] = ' ';
+                }
+
+                for (int row = counter; row < matrix.Length; row++)
+                {
+                    matrix[row][col] = elements.Dequeue();
+                }
+            }
+        }
+
+        private static void Shoot(char[][] matrix, int[] target)
+        {
+            int targetRow = target[0];
+            int targetCol = target[1];
+            int radius = target[2];
 
             for (int row = 0; row < matrix.Length; row++)
             {
-                //matrix[row] = matrix[colSize];
-                char[] snake = Console.ReadLine()
-                    .ToArray();
                 for (int col = 0; col < matrix[row].Length; col++)
                 {
+                    bool isInside = Math.Pow((targetRow - row), 2) + Math.Pow((targetCol - col), 2) <= Math.Pow(radius, 2);
 
+                    if (isInside)
+                    {
+                        matrix[row][col] = ' ';
+                    }
                 }
             }
+        }
 
-            foreach (var item in matrix)
+        private static void Print(char[][] matrix)
+        {
+            foreach (var row in matrix)
             {
-                Console.Write(item);
+                Console.WriteLine(String.Join("", row));
             }
-            Console.WriteLine();
+        }
+
+        private static void GetMatrix(char[][] matrix, int cols, string snake)
+        {
+            int counter = 0;
+            bool isLeft = true;
+
+            for (int row = matrix.Length - 1; row >= 0; row--)
+            {
+                matrix[row] = new char[cols];
+
+                if (isLeft)
+                {
+                    for (int col = matrix[row].Length - 1; col >= 0; col--)
+                    {
+                        counter = SetLetter(matrix, snake, counter, row, col);
+                    }
+
+                    isLeft = false;
+                }
+                else
+                {
+                    for (int col = 0; col < matrix[row].Length; col++)
+                    {
+                        counter = SetLetter(matrix, snake, counter, row, col);
+                    }
+
+                    isLeft = true;
+                }
+            }
+        }
+
+        private static int SetLetter(char[][] matrix, string snake, int counter, int row, int col)
+        {
+            if (counter > snake.Length - 1)
+            {
+                counter = 0;
+            }
+            matrix[row][col] = snake[counter++];
+            return counter;
         }
     }
 }
