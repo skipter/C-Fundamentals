2455b72aa661defd1e7550decffced9948602de7
Boris Mandzhukov <skipter@abv.bg>
2018-09-30 15:18:11 +0300

    Add setsAndDictionaries exercises - one left .

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/07.TheVLogger/TheVLogger.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.TheVLogger
+{
+    class TheVLogger
+    {
+        static void Main()
+        {
+            var inputData = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Dictionary<string, Dictionary<string, SortedSet<string>>> vloggers = 
+                new Dictionary<string, Dictionary<string, SortedSet<string>>>();
+
+            
+            //int counter = 0;
+
+            while (inputData[0] != "Statistics")
+            {
+                string firstUsername = inputData[0];
+                string command = inputData[1];
+                string secondUsername = inputData[2];
+
+                if (command == "joined")
+                {
+                    if (!vloggers.ContainsKey(firstUsername))
+                    {   //Add user and add to each of them the two keys(key fot the second dictionary.
+                        vloggers.Add(firstUsername, new Dictionary<string, SortedSet<string>>());
+                        vloggers[firstUsername].Add("following", new SortedSet<string>());
+                        vloggers[firstUsername].Add("followers", new SortedSet<string>());
+                    }
+                }
+                else if (command == "followed")
+                {
+                    bool isSamePerson = firstUsername == secondUsername;
+                    if (vloggers.ContainsKey(firstUsername) && vloggers.ContainsKey(secondUsername) && !isSamePerson) //Firstname and secondname and they are different people !
+                    {   //Tricky logic.
+                        vloggers[firstUsername]["following"].Add(secondUsername);
+                        vloggers[secondUsername]["followers"].Add(firstUsername);
+                    }
+                }
+
+
+                inputData = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            }
+            //Print result
+            Console.WriteLine($"The V-Logger has a total of {vloggers.Count} vloggers in its logs.");
+            //"followers" and "following" are keys.
+            var sortedVloggers = vloggers.OrderByDescending(x => x.Value["followers"].Count)
+                .ThenBy(x => x.Value["following"].Count);
+
+            int counter = 1;
+
+            foreach (var item in sortedVloggers)
+            {
+                
+                Console.WriteLine($"{counter}. {item.Key} : {item.Value["followers"].Count} followers, " +
+                    $"{item.Value["following"].Count} following");
+
+                if (counter == 1)
+                {
+                    foreach (var followerName in item.Value["followers"])
+                    {
+                        Console.WriteLine($"*  {followerName}");
+                    }
+                }
+                counter++;
+            }
+        }
+    }
+}
---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/08.Ranking/Ranking.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.Ranking
+{
+    class Ranking
+    {
+        static void Main()
+        {
+            Dictionary<string, string> contestsInfo = new Dictionary<string, string>();
+
+
+
+            string[] input = Console.ReadLine()
+                .Split(":")
+                .ToArray();
+
+            while (input[0] != "end of contests")
+            {
+                string contest = input[0];
+                string password = input[1];
+
+                if (!contestsInfo.ContainsKey(contest))
+                {
+                    contestsInfo.Add(contest, password);
+                }
+            }
+
+            var secondInput = Console.ReadLine()
+                .Split("=>")
+                .ToArray();
+            while (secondInput[0] != "end of submissions")
+            {
+                string examName = secondInput[0];
+                string passExam = secondInput[1];
+                string username = secondInput[2];
+                int points = int.Parse(secondInput[3]);
+            }
+        }
+    }
+}
