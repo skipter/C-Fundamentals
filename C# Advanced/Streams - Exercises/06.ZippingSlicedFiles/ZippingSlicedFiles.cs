c9cfe8851661a49b7f1b39338d1eaf1f4b7c03f2
Boris Mandzhukov <skipter@abv.bg>
2018-10-06 11:03:32 +0300

    Finish with streams for homework.

--- C# Advanced/Streams - Exercises/06.ZippingSlicedFiles/ZippingSlicedFiles.cs
+++ C# Advanced/Streams - Exercises/06.ZippingSlicedFiles/ZippingSlicedFiles.cs
@@ -1,12 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 
 namespace _06.ZippingSlicedFiles
 {
     class ZippingSlicedFiles
     {
+        static List<string> paths;
+
         static void Main()
         {
-            
+            paths = new List<string>();
+
+            string sourceFile = "..//..//..//..//files//sliceMe.mp4";
+            string outputDestination = "..//..//..//..//files//";
+            int partsToSlice = 4;
+
+            Slice(sourceFile, outputDestination, partsToSlice);
+
+            Assemble(paths, outputDestination);
+        }
+
+        static void Slice(string sourceFile, string destinationDirectory, int parts)
+        {
+            //To read file.
+            using (FileStream readFile = new FileStream(sourceFile, FileMode.Open))
+            {
+                long size = readFile.Length / parts + readFile.Length % parts;
+                byte[] buffer = new byte[size];
+
+                for (int i = 0; i < parts; i++)
+                {
+                    string destinationPath = destinationDirectory + $"Path-{i}.mp4";
+
+                    paths.Add(destinationPath);
+
+                    using (FileStream writeFile = new FileStream(destinationPath, FileMode.Create))
+                    {
+                        int bytesCount = readFile.Read(buffer, 0, buffer.Length);
+                        writeFile.Write(buffer, 0, buffer.Length);
+                    }
+
+                    using (GZipStream gz = new GZipStream(new FileStream(destinationPath + ".gz", FileMode.Create), CompressionMode.Compress, false))
+                    {
+                        gz.Write(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+        }
+
+        static void Assemble(List<string> files, string destinationDirectory)
+        {
+            byte[] buffer = new byte[1024];
+
+            destinationDirectory += "assembled.mp4";
+
+            using (FileStream writeFile = new FileStream(destinationDirectory, FileMode.Create))
+            {
+                foreach (var path in paths)
+                {
+
+                    using (GZipStream gzZip = new GZipStream(new FileStream(path + ".gz", FileMode.Open), CompressionMode.Decompress))
+                    {
+                        while (true)
+                        {
+                            int bytesCount = gzZip.Read(buffer, 0, buffer.Length);
+
+                            if (bytesCount == 0)
+                            {
+                                break;
+                            }
+                            writeFile.Write(buffer, 0, bytesCount);
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- C# Advanced/Streams - Exercises/07.DirectoryTraversal/DirectoryTraversal.cs
+++ C# Advanced/Streams - Exercises/07.DirectoryTraversal/DirectoryTraversal.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace _07.DirectoryTraversal
 {
@@ -6,7 +9,45 @@ namespace _07.DirectoryTraversal
     {
         static void Main()
         {
-            
+            Dictionary<string, Dictionary<string, decimal>> directoryInfo = new Dictionary<string, Dictionary<string, decimal>>();
+
+            string path = "..//..//..//";
+            string[] files = Directory.GetFiles(path,
+                "*.*",
+                SearchOption.TopDirectoryOnly);
+
+            foreach (string file in files)
+            {
+                FileStream currentFile = File.Open(file, FileMode.Open);
+                var fileName = Path.GetFileName(file);
+                var extension = Path.GetExtension(file);
+                Decimal fileSize = Decimal.Divide(currentFile.Length, 1024);
+
+                if (!directoryInfo.ContainsKey(extension))
+                {
+                    directoryInfo.Add(extension, new Dictionary<string, decimal>());
+                }
+
+                if (!directoryInfo[extension].ContainsKey(fileName))
+                {
+                    directoryInfo[extension].Add(fileName, fileSize);
+                }
+            }
+
+            string pathDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\report.txt";
+
+            using (StreamWriter writer = new StreamWriter(pathDesktop))
+            {
+                foreach (var kvp in directoryInfo.OrderByDescending(x => x.Value.Count).ThenBy(k => k.Key))
+                {
+                    writer.WriteLine(kvp.Key);
+
+                    foreach (var fileInfo in kvp.Value.OrderBy(x => x.Key))
+                    {
+                        writer.WriteLine($"--{fileInfo.Key} - {fileInfo.Value:F2}kb");
+                    }
+                }
+            }
         }
     }
 }
