130ca41e090006e2e4b7987da9fb65841d45c53b
Boris Mandzhukov <skipter@abv.bg>
2018-10-19 22:12:05 +0300

    OOP exercises.

---
+++ C# OOP Basic/Defining Classes - Exercises/01.DefinaAClassPerson/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Person person = new Person();
+
+            person.Name = "Ivan"; //Console.ReadLine();
+            person.Age = 15;
+
+            //Console.WriteLine($"Your name is {person.Name} and your age is {person.Age}");
+        }
+    }
+}
---
+++ C# OOP Basic/Defining Classes - Exercises/03.OldestFamilyMember/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.OldestFamilyMember
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        //Properties
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+
+        //Constructors
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(int age)
+            : this()            // :this() -> chaining constructors
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+            : this(age)
+        {
+            this.Name = name;
+        }
+    }
+}
