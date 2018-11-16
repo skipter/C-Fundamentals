dd0a7ad7e8fe93df13166a85c7e1b6daadcd2bff
Boris Mandzhukov <skipter@abv.bg>
2018-11-16 12:14:13 +0200

    Add exam prep 2.

---
+++ C# OOP Basic/ExamPreparation II/DungeonsAndCodeWizards/Entity/Item/HealthPotion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsAndCodeWizards.Entity.Item
+{
+    public class HealthPotion : Item
+    {
+        private const int HealthPotionWeight = 5;
+
+        public HealthPotion() 
+            : base(HealthPotionWeight)
+        {
+        }
+
+        public void AffectCharacter(Character character)
+        {
+
+        }
+    }
+}
---
+++ C# OOP Basic/ExamPreparation II/DungeonsAndCodeWizards/Entity/Item/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsAndCodeWizards.Entity.Item
+{
+    public abstract class Item
+    {
+        private int weight;
+
+        public Item(int weight)
+        {
+            this.Weight = weight;
+        }
+
+        public int Weight { get => weight; private set => weight = value; }
+
+        public void AffectCharacter(Character character)
+        {
+
+        }
+    }
+}
