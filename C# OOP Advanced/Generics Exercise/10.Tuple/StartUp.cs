3c145fc215e9d09f17ea5927d16ce0b87269846a
Boris Mandzhukov <skipter@abv.bg>
2018-11-30 00:02:23 +0200

    Add iterators and comparators.

--- C# OOP Advanced/Generics Exercise/10.Tuple/StartUp.cs
+++ C# OOP Advanced/Generics Exercise/10.Tuple/StartUp.cs
@@ -6,7 +6,28 @@ namespace _10.Tuple
     {
         public static void Main()
         {
+            string[] personInfo = Console.ReadLine().Split();
 
+            string fullName = personInfo[0] + " " + personInfo[1];
+            string adress = personInfo[2];
+
+            string[] beerInfo = Console.ReadLine().Split();
+
+            string name = beerInfo[0];
+            int litters = int.Parse(beerInfo[1]);
+
+            string[] specialNumbers = Console.ReadLine().Split();
+
+            int specialNum = int.Parse(specialNumbers[0]);
+            double specialDouble = double.Parse(specialNumbers[1]);
+
+            SpecialTuple<string, string> personAdress = new SpecialTuple<string, string>(fullName, adress);
+            SpecialTuple<string, int> beerLitters = new SpecialTuple<string, int>(name, litters);
+            SpecialTuple<int, double> specialNums = new SpecialTuple<int, double>(specialNum, specialDouble);
+
+            Console.WriteLine(personAdress);
+            Console.WriteLine(beerLitters);
+            Console.WriteLine(specialNums);
         }
     }
 }
---
+++ C# OOP Advanced/Generics Exercise/11.Threeuple/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _11.Threeuple
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] personInfo = Console.ReadLine().Split();
+
+            string fullName = personInfo[0] + " " + personInfo[1];
+            string adress = personInfo[2];
+            string town = personInfo[3];
+
+            string[] beerInfo = Console.ReadLine().Split();
+
+            string name = beerInfo[0];
+            int litters = int.Parse(beerInfo[1]);
+            bool drunkOrNot = beerInfo[2] == "drunk"? true : false;
+
+            string[] bankInfo = Console.ReadLine().Split();
+
+            string nameAccount = bankInfo[0];
+            double cash = double.Parse(bankInfo[1]);
+            string bankName = bankInfo[2];
+
+            Threeuple<string, string, string> personAdress = new Threeuple<string, string, string>(fullName, adress, town);
+            Threeuple<string, int, bool> beerLitters = new Threeuple<string, int, bool>(name, litters, drunkOrNot);
+            Threeuple<string, double, string> specialNums = new Threeuple<string, double, string>(nameAccount, cash, bankName);
+
+            Console.WriteLine(personAdress);
+            Console.WriteLine(beerLitters);
+            Console.WriteLine(specialNums);
+        }
+    }
+}
