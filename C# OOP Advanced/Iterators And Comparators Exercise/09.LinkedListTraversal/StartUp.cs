928f7538b170120db6b6d0a468eafde7dbbfd762
Boris Mandzhukov <skipter@abv.bg>
2018-12-05 22:34:24 +0200

    add exercise

---
+++ C# OOP Advanced/Iterators And Comparators Exercise/09.LinkedListTraversal/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09.LinkedListTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
