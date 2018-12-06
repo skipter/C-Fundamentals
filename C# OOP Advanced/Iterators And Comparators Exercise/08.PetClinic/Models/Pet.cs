5eceb1ef031154093d427eccd7593aedb5207bfe
Boris Mandzhukov <skipter@abv.bg>
2018-12-06 09:36:29 +0200

    finish iterators and comperators exercises.

--- C# OOP Advanced/Iterators And Comparators Exercise/08.PetClinic/Models/Pet.cs
+++ C# OOP Advanced/Iterators And Comparators Exercise/08.PetClinic/Models/Pet.cs
@@ -2,5 +2,26 @@
 {
     public class Pet
     {
+        private string name;
+        private int age;
+        private string kind;
+
+        public Pet(string name, int age, string kind)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Kind = kind;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Kind { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Age} {this.Kind}";
+        }
     }
 }
--- C# OOP Advanced/Iterators And Comparators Exercise/09.LinkedListTraversal/StartUp.cs
+++ C# OOP Advanced/Iterators And Comparators Exercise/09.LinkedListTraversal/StartUp.cs
@@ -1,12 +1,34 @@
-﻿using System;
-
-namespace _09.LinkedListTraversal
+﻿namespace _09.LinkedListTraversal
 {
-    class Program
+    using System;
+
+    public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var linkedList = new LinkedList<int>();
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var commandArgs = Console.ReadLine().Split();
+                var command = commandArgs[0];
+
+                switch (command)
+                {
+                    case "Add":
+                        var elementToAdd = int.Parse(commandArgs[1]);
+                        linkedList.Add(elementToAdd);
+                        break;
+                    case "Remove":
+                        var elementToRemove = int.Parse(commandArgs[1]);
+                        linkedList.Remove(elementToRemove);
+                        break;
+                }
+            }
+
+            Console.WriteLine(linkedList.Count);
+            Console.WriteLine(string.Join(" ", linkedList));
         }
     }
 }
