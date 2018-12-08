ba12fcc66611e9e840f37b5911b17e845061eb5f
Boris Mandzhukov <skipter@abv.bg>
2018-12-09 00:32:06 +0200

    Add reflection exercises.

---
+++ C# OOP Advanced/ReflectionAndAttributesExercise/01.HarvestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,64 @@
+﻿namespace P01_HarvestingFields
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Type type = typeof(HarvestingFields);
+
+            FieldInfo[] fields = type.GetFields(BindingFlags.Public |
+                BindingFlags.NonPublic | BindingFlags.Instance);
+
+            while (input != "HARVEST")
+            {
+                // switch (input)
+                // {
+                //     case "private":
+                //         break;
+                //     case "public":
+                //         break;
+                //     case "protected":
+                //         break;
+                //
+                //     default:
+                //         break;
+                // }
+
+                
+
+                if (input != "all")
+                {
+                    //This reflection replaces the swithc case method
+                    FieldInfo[] fieldToPrint = fields
+                    .Where(t => t.Attributes
+                    .ToString()
+                    .ToLower()
+                    .Replace("family", "protected") == input)
+                    .ToArray();
+
+                    foreach (var field in fieldToPrint)
+                    {
+                        Console.WriteLine($"{field.Attributes.ToString().ToLower().Replace("family", "protected")} " +
+                            $"{field.FieldType.Name} {field.Name}");
+                    }
+                }
+                else
+                {
+                    foreach (var field in fields)
+                    {
+                        Console.WriteLine($"{field.Attributes.ToString().ToLower().Replace("family", "protected")} " +
+                            $"{field.FieldType.Name} {field.Name}");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ C# OOP Advanced/ReflectionAndAttributesExercise/02.BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,41 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            Type type = typeof(BlackBoxInteger);
+
+            //Create an instance because methods in blackBoxInteger are not static.
+            var box = (BlackBoxInteger)Activator.CreateInstance(type, true);
+
+            string data = Console.ReadLine();
+
+            while (data != "END")
+            {
+                string[] inputArgs = data.Split("_");
+                string command = inputArgs[0];
+                int number = int.Parse(inputArgs[1]);
+
+                MethodInfo field = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+                    .First(m => m.Name == command);
+
+                field.Invoke(box, new object[] { number });
+
+                var result = type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
+                    .First(f => f.Name == "innerValue").GetValue(box);
+
+                Console.WriteLine(result);
+
+                data = Console.ReadLine();
+            }
+            
+
+            
+        }
+    }
+}
